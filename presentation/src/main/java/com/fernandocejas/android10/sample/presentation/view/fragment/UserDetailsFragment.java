3b910c81ec560aaadcdb81c6ceb15d9916f09f81
Emanuel <koky06faith@gmail.com>
2017-02-28 11:22:35 -0500

    dos

--- presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/fragment/UserDetailsFragment.java
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/fragment/UserDetailsFragment.java
@@ -1,7 +1,3 @@
-/**
- * Copyright (C) 2014 android10.org. All rights reserved.
- * @author Fernando Cejas (the android10 coder)
- */
 package com.fernandocejas.android10.sample.presentation.view.fragment;
 
 import android.content.Context;
